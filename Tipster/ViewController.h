2c8057362d54334c5959f130dd5370b1ffe0bcbb
Esther Brown <estherb@fb.com>
2019-06-25 14:12:16 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by estherb on 6/25/19.
+//  Copyright © 2019 estherb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
